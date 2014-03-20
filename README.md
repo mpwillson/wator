f3c45b5f1285b9bb5845156af2aa86efcf7082e6
Mark Willson <cdr.nil@gmail.com>
2014-03-20 14:35:09 +0000

    Initial skeleton commit

---
+++ README.md
@@ -0,0 +1,26 @@
+# wator
+
+A Clojurescript implementation of the Wator simulation, as described
+in The Armchair Universe, by A. K. Dwedney.
+
+## Installation
+
+Download from http://github.com/mpwillson/wator
+
+## Usage
+
+FIXME: explanation
+
+### Bugs
+
+...
+
+### Any Other Sections
+### That You Think
+### Might be Useful
+
+## License
+
+Copyright © 2014 Mark Willson
+
+Distributed under the Eclipse Public License, the same as Clojure.
---
+++ project.clj
@@ -0,0 +1,14 @@
+(defproject wator "0.1.0-SNAPSHOT"
+  :description "Clojurescript Wator simulation, as described in The Armchair Universe, by A. K. Dwedney."
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :plugins [[lein-cljsbuild "1.0.2"]]
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [org.clojure/clojurescript "0.0-2173"]
+                 [org.clojure/tools.nrepl "0.2.3"]]
+  :source-paths ["src/clj"]
+  :cljsbuild {:builds [{:source-paths ["src/cljs"]
+                        :compiler {:output-to "resources/public/wator.js"
+                                   :optimizations :whitespace
+                                   :pretty-print true}}]})
